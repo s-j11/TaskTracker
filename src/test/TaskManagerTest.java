5d5232f7a929723363b4142212c739deaaa7b914
Sergey <s-mvc@yandex.ru>
2022-07-05 23:55:05 +0300

    refactor: версия 2.612

--- src/test/TaskManagerTest.java
+++ src/test/TaskManagerTest.java
@@ -10,7 +10,6 @@ import model.Task;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.util.*;
 
