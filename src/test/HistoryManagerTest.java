74713c85acdfca4bb227238c369a85366ae2eb18
Sergey <s-mvc@yandex.ru>
2022-07-10 21:37:18 +0300

    refactor: версия 2.618

--- src/test/HistoryManagerTest.java
+++ src/test/HistoryManagerTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class HistoryManagerTest {
     private Managers managers = new Managers();
     private TaskManager inMemoryTaskManager = managers.getDefault();
-    private  HistoryManager historyManager =  inMemoryTaskManager.getHistoryManager();
+    private  HistoryManager historyManager =  new InMemoryHistoryManager();
     private List<Node> historyList = new ArrayList<>();
     private  Node<Task> node;
 
@@ -36,7 +36,7 @@ class HistoryManagerTest {
                 "Разработка класа тестирования", 2);
     }
     @Test
-    void shouldAdd() {
+    public void shouldAdd() {
         Task task = new Task("Проектирование", "Проектирование ПО", 1, Status.NEW);
         Assertions.assertEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
         historyManager.add(task);
@@ -46,7 +46,7 @@ class HistoryManagerTest {
     }
 
     @Test
-    void shouldRemove() {
+    public void shouldRemove() {
         Task task = new Task("Проектирование", "Проектирование ПО", 1, Status.NEW);
         node = new Node<Task>(task);
         historyManager.add(task);
@@ -54,18 +54,144 @@ class HistoryManagerTest {
         Assertions.assertNotNull(inMemoryTaskManager.getHistoryManager().getHistory());
         Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
         historyManager.remove(node);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getHistoryManager().remove(node);
         Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
     }
 
     @Test
-    void shouldGetHistory() {
+    public void shouldGetHistory() {
+        Task task = new Task("Проектирование", "Проектирование ПО", 1, Status.NEW);
+        Assertions.assertEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.add(task);
+        Assertions.assertNotEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getTaskById(1);
+        Assertions.assertNotNull(inMemoryTaskManager.getHistoryManager().getHistory());
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
     }
 
     @Test
-    void shouldClearHistory() {
+    public void shouldClearHistory() {
+        Task task = new Task("Проектирование", "Проектирование ПО", 1, Status.NEW);
+        Map<Integer, EpicTask> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        EpicTask epicTask = new EpicTask("Тестирование", "Разработка тестирования", 2, Status.NEW,
+                list);
+        Assertions.assertEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.add(task);
+        Assertions.assertNotEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getTaskById(1);
+        Assertions.assertNotNull(inMemoryTaskManager.getHistoryManager().getHistory());
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.add(epicTask);
+        inMemoryTaskManager.getEpicTaskById(2);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.removeAllNode();
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getHistoryManager().removeAllNode();
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
     }
 
     @Test
-    void shouldRemoveAllNode() {
+    public void shouldRemoveAllNode() {
+        Task task = new Task("Проектирование", "Проектирование ПО", 1, Status.NEW);
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        EpicTask epicTask = new EpicTask("Тестирование", "Разработка тестирования", 2, Status.NEW,
+                list);
+        Assertions.assertEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.add(task);
+        Assertions.assertNotEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getTaskById(1);
+        Assertions.assertNotNull(inMemoryTaskManager.getHistoryManager().getHistory());
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.add(epicTask);
+        inMemoryTaskManager.getEpicTaskById(2);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.removeAllNode();
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getHistoryManager().removeAllNode();
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
     }
+
+    @Test
+    public void shouldEmptyHistory(){
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+    }
+
+    @Test
+    public void shouldDuplicate(){
+        Task task = new Task("Проектирование", "Проектирование ПО", 1, Status.NEW);
+        Assertions.assertEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        EpicTask epicTask = new EpicTask("Тестирование", "Разработка тестирования", 2, Status.NEW,
+                list);
+        historyManager.add(task);
+        historyManager.add(epicTask);
+        inMemoryTaskManager.getTaskById(1);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getEpicTaskById(2);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        historyManager.add(task);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getTaskById(1);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+    }
+
+    @Test
+    public void shouldDeleteBeginMiddleEnd(){
+        Task task = new Task("Проектирование", "Проектирование ПО", 1, Status.NEW);
+        Assertions.assertEquals(historyManager.getHistory(), inMemoryTaskManager.getHistoryManager().getHistory());
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        EpicTask epicTask = new EpicTask("Тестирование", "Разработка тестирования", 2, Status.NEW,
+                list);
+        SubTask subTask = new SubTask("Разработка меню", "Разработка класса меню", 3, Status.NEW,
+                2);
+        SubTask subTask1 = new SubTask("Разработка логики", "Разработка класса логики", 4, Status.NEW,
+                2);
+        SubTask subTask2 = new SubTask("Класс тестирования", "Разработка класа тестирования", 5, Status.NEW,
+                2);
+
+        historyManager.add(task);
+        historyManager.add(epicTask);
+        historyManager.add(subTask);
+        historyManager.add(subTask1);
+        historyManager.add(subTask2);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getTaskById(1);
+        inMemoryTaskManager.getEpicTaskById(2);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getSubTaskById(3);
+        inMemoryTaskManager.getSubTaskById(4);
+        inMemoryTaskManager.getSubTaskById(5);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        node = new Node<Task>(task);
+        historyManager.remove(node);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getHistoryManager().remove(node);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        node = new Node<Task>(subTask);
+        historyManager.remove(node);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getHistoryManager().remove(node);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        node = new Node<Task>(subTask2);
+        historyManager.remove(node);
+        Assertions.assertNotEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+        inMemoryTaskManager.getHistoryManager().remove(node);
+        Assertions.assertEquals(historyManager.getHistory(),inMemoryTaskManager.getHistoryManager().getHistory());
+    }
+
+
 }
